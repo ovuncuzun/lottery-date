fa863074cf63019a2f2a28941f33f89efb6f70f7
nanokozmos <ovuncuzun@gmail.com>
2020-04-20 11:27:35 +0100

    checking the hour and refactoring the code

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ function getLotteryDrawDate(date) {
     var lotteryDrawDate = new Date(date || new Date());
     if (lotteryDrawDate.getDay() < 4) {
         // get next wednesday
-        lotteryDrawDate.setDate(lotteryDrawDate.getDate() + (3 - lotteryDrawDate.getDay()));
+        getSpecificDay(lotteryDrawDate, 3);
+
+        // get next saturday
+        if (lotteryDrawDate.getHours() > 20) {
+            getSpecificDay(lotteryDrawDate, 6);
+        }
     } else {
         // get next saturday
-        lotteryDrawDate.setDate(lotteryDrawDate.getDate() + (6 - lotteryDrawDate.getDay()));
+        getSpecificDay(lotteryDrawDate, 6);
     }
-    return lotteryDrawDate.toLocaleDateString();
+    lotteryDrawDate.setHours(20, 00, 00);
+    return lotteryDrawDate.toLocaleString();
+}
+
+function getSpecificDay(lotteryDrawDate, iterator) {
+    return lotteryDrawDate.setDate(lotteryDrawDate.getDate() + (iterator - lotteryDrawDate.getDay()));
 }
 
 var tomorrow = new Date();
@@ -19,3 +29,6 @@ nextThursday.setDate(new Date().getDate() + 3);
 console.log(getLotteryDrawDate(new Date()));
 console.log(getLotteryDrawDate(tomorrow));
 console.log(getLotteryDrawDate(nextThursday));
+console.log(getLotteryDrawDate(new Date("2020-04-22 21:00")))
+
+
